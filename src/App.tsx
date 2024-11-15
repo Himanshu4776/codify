64c0b2f4935ec47fa7d51fb7d59329bd7322975e
himanshu4776 <hk91619@gmail.com>
2024-11-15 13:11:17 +0530

    feat: Editor UI and tailwind css setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./styles.css";
-import { Editor } from "./Editor";
+import { Editor } from "./components/Editor";
 import { useAtomValue, useSetAtom } from "jotai";
 import { langTypeAtom } from "./hooks/constants";
 
